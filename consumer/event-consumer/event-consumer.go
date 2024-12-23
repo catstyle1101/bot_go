2446acd1690b4a2d0d4870739246d583f659dd35
Anton Masyuk <a.masyuk@comagic.dev>
2024-12-23 17:53:38 +0500

    add goroutines to process events

--- consumer/event-consumer/event-consumer.go
+++ consumer/event-consumer/event-consumer.go
@@ -3,6 +3,7 @@ package event_consumer
 import (
 	"bot_go/events"
 	"log"
+	"sync"
 	"time"
 )
 
@@ -44,18 +45,23 @@ func (c *Consumer) Start() error {
 TODO:
 1. Потеря событий: ретраи, возвращение в хранилище, фоллбек, подтверждение для fetcher
 2. обработка всей пачки: останавливаться после первой ошибки, счетчик ошибок
-3. Параллельная обработка (sync.WaitGroup{})
+3. Параллельная обработка (sync.WaitGroup{}) ✅
 */
 
 func (c *Consumer) handleEvents(events []events.Event) error {
+	var wg sync.WaitGroup
+
 	for _, event := range events {
 		log.Printf("[INFO] consumer received event: %s", event)
+		wg.Add(1)
 
-		if err := c.processor.Process(event); err != nil {
-			log.Printf("[ERR] consumer: %s", err.Error())
-
-			continue
-		}
+		go func() {
+			defer wg.Done()
+			if err := c.processor.Process(event); err != nil {
+				log.Printf("[ERR] consumer: %s", err.Error())
+			}
+		}()
 	}
+	wg.Wait()
 	return nil
 }
